f5da2eea1fec35d3bfcceafe3ade2c6f1d1e4f97
ShiArtDark <thguichot@gmail.com>
2025-05-14 13:02:11 +0200

    Ajout de la classe Sommet

---
+++ src/main/java/org/APPLI/modele/Sommet.java
@@ -0,0 +1,33 @@
+package org.APPLI.modele;
+
+public class Sommet {
+    private String name;
+    private int pass;
+    private int id;
+    // =================== Constructeur =====================
+    public Sommet(String _name, int _pass, int _id) {
+        name = _name;
+        pass = _pass;
+        id = _id;
+    }   
+
+
+    // =================== Get/Set ==========================
+    public void setName(String _name) {
+        name = _name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setPass(int _pass) {
+        pass = _pass;
+    }
+
+    public int getPass() {
+        return pass;
+    }
+
+    
+}
