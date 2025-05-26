0926bced3a708cb7582bf0370b3cdd139ff15fb3
Kyln78 <kylliandjoua@gmail.com>
2025-05-26 15:36:22 +0200

    dczc

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.example.saejsplusclb {
+
+    requires javafx.fxml;
+    requires org.controlsfx.controls;
+    requires javatuples;
+
+    opens org.APPLI to javafx.fxml; // ← Important pour FXML
+    exports org.APPLI;              // ← Important pour que JavaFX accède à Main
+}
