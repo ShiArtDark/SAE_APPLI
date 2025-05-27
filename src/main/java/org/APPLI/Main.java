8763041249d0b80306e0dbd1536f7d77e26dff06
Kyllian Djoua <22400112@me.uvsq.fr>
2025-05-27 14:13:41 +0200

    Clean du main

--- src/main/java/org/APPLI/Main.java
+++ src/main/java/org/APPLI/Main.java
@@ -35,36 +35,7 @@ public class Main extends Application {
 
 
     public static void main(String[] args) {
-        try {
-            
-             
-            for (int i =0 ; i < 9; i++) {
-                String scenario = "scenario_"+i+".txt";
-                TreeMap<Sommet, ArrayList<Sommet>> map = FileManager.toGraph(scenario);
-                Graphe graphe = new Graphe(map);
-                ArrayList<Sommet> chemin = graphe.triTopologique(Graphe.FIRST);
-                ArrayList<Sommet> cheminDist = graphe.triTopologique(Graphe.DISTANCE);
-                ArrayList<Sommet> cheminDegr = graphe.triTopologique(Graphe.DEGREE);
-                
-                System.out.println("Scenario FIR"+i+" : "+ graphe.GetDistance(chemin));
-                System.out.println("Scenario DIS"+i+" : "+ graphe.GetDistance(cheminDist));
-                System.out.println("Scenario DEG"+i+" : "+ graphe.GetDistance(cheminDegr)+"\n");
-                
-
-
-                
-                // System.out.println(graphe.topologogiqueRecursif(graphe.getEntrant(), graphe.getSource() ,new ArrayList<>()));
-            }
-            
-            String scenario = "scenario_"+0+".txt";
-            TreeMap<Sommet, ArrayList<Sommet>> map = FileManager.toGraph(scenario);
-            Graphe graphe = new Graphe(map);
-            System.out.println((graphe.triTopologiqueRecursif())+"\n");
-
-        
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
+        launch(args);
 
 
     }
